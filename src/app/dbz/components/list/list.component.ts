b9621fca498361d9e2a0eecedabd5c0186a17052
VN - Alvaro Hdz Valero <alvaro.hernandezv@viewnext.com>
2024-01-31 08:42:54 +0100

    S05 - @Input

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  //* Establece que la lista de caracteres es una propiedad que puede ser enviada
+  //* desde un componente (MainPageComponent) a otro (ListComponent)
+  //* Al definirlo con contenido, si no recibe ninguna entrada mostrará ese contenido
+  //* por defecto.
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: 'Trunks',
+      power: 10
+    }
+  ]
 
 }
